6da0a1c6440c3ac4d4f6f0bb27017b3370c0d484
HLB <mzeelovegeneral@gmail.com>
2023-07-13 22:41:19 +0100

    Welldone

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 printf("Size of char: %d bytes(s)\n", sizeof(char));
 printf("Size of int: %d bytes(s)\n", sizeof(int));
 printf("Size of long int: %d bytes(s)\n", (long) sizeof(int));
-printf("Size of long long int: %d bytes(s)\n", (long long) sizeof(char));
+printf("Size of long long int: %d bytes(s)\n", (long long) sizeof(int));
 printf("Size of float: %d bytes(s)\n", sizeof(float));
 return (0);
 }
