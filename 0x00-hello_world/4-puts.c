7244c6c2e40da9ab0ea75a4b205f8ef61af57477
HLB <mzeelovegeneral@gmail.com>
2023-07-13 17:36:48 +0100

    Welldone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 # include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle\n");
 return (0);
 }
