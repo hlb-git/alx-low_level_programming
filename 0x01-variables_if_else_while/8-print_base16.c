ae915f1cdfb618d4fce6e9ba7b9bfc28d2427e7d
HLB <mzeelovegeneral@gmail.com>
2023-07-14 21:42:50 +0100

    Welldone

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: the code terminates here
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+	putchar(a)
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
