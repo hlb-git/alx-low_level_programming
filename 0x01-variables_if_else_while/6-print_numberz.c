584dc1e5a438dc1afcdbe674c9521ef8b35c3a9c
HLB <mzeelovegeneral@gmail.com>
2023-07-14 19:55:40 +0100

    Welldone

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: the code terminates here
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+	putchar(a);
+	}
+	putchar("\n");
+	return (0);
+}
