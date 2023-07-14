740be969161fb23177324a816e00826e353371fa
HLB <mzeelovegeneral@gmail.com>
2023-07-14 17:03:40 +0100

    Welldone

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: the code terminates here
+*/
+
+int main(void)
+{
+char a;
+
+for (a = 'a', a < 'z', a++)
+	putchar(a);
+	putchar(\n);
+	return (0);
+}
