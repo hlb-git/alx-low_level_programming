1acb7e002ec21ad16c88062a4a6860198a894fc9
HLB <mzeelovegeneral@gmail.com>
2023-07-14 18:21:49 +0100

    Welldone

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: the code terminates here
+*/
+
+int main(void)
+{
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+	if (a == 'q' || a == 'e')
+	continue;
+	putchar(a);
+putchar('\n');
+return (0);
+}
