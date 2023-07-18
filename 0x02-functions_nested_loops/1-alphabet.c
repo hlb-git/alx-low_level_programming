2c5adff10ca29e9380a680f57513b665a3ef2d46
HLB <mzeelovegeneral@gmail.com>
2023-07-18 16:39:54 +0100

    Welldone

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main - entry point
+*
+*Return: the program output
+*/
+
+void print_alphabet(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++);
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+	return (0);
+}
