9a5e66d5f301e5623287af16e86df9870cd5b58f
HLB <mzeelovegeneral@gmail.com>
2023-07-18 22:30:48 +0100

    Welldone

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*_abs - entry point
+*@n: an argument
+*Return: the program output
+*/
+
+
+int _abs(int n)
+
+{
+	if (n < 0)
+	{
+		n = n + (n*2);
+		return (n);
+	}
+	else
+	{
+		n = n
+		return (n);
+	}
+	_putchar('\n');
+}
