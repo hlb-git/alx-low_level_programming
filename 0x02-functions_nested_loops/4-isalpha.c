cd85cf32cea26a21d281af789e8173a73f36178c
HLB <mzeelovegeneral@gmail.com>
2023-07-18 20:10:34 +0100

    Welldone

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_islower - entry point
+*@c: an argument
+*Return: the program output
+*/
+
+
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+}
