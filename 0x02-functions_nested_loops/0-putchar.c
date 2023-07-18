75ab77eecb99957a6e18f5cbbe5864ba73b3a28b
HLB <mzeelovegeneral@gmail.com>
2023-07-18 13:23:39 +0100

    Welldone

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/**
+*main - entry point
+*
+*Return: the program output
+*/
+
+int main(void)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r\n");
+	return (0);
+}
