22dec00dcd6f3457592cd595e3fe4ace13e20e0b
HLB <mzeelovegeneral@gmail.com>
2023-07-19 11:16:52 +0100

    Welldone

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*times_table - entry point
+*Return: the program output
+*/
+
+void times_table(void)
+{
+
+	int l, a;
+	for (l = 0; l < 10; l++)
+	{
+		_putchar('0');
+		_putchar(l);
+
+		a = l;
+
+		for (a = l; a < 10; a += a)
+		{
+			_putchar(a);
+
+		}
+		_putchar('\n');
+	}
+}
