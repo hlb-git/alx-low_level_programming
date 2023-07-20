c733a245f46c37419db7b74effaf66a9dcbba6e0
HLB <mzeelovegeneral@gmail.com>
2023-07-20 14:09:22 +0100

    Welldone

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/**
+* _isupper - takes a date and prints how many days are
+* @c: month in number format
+* Return: void
+*/
+
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
