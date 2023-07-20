56fd9457b255cd2aac12b6a8835716d57261e7e8
HLB <mzeelovegeneral@gmail.com>
2023-07-20 18:59:16 +0100

    Welldone

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_line - prints 0 to 9
+* @n: the integer argument
+*/
+
+
+void print_line(int n)
+{
+
+	for (n = 1; n <= n; n++)
+	{
+		if (n <= 0)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+
+}
