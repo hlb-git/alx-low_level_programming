e080081baa91299d807b9cb6a8cb89d2d1e7ca73
HLB <mzeelovegeneral@gmail.com>
2023-07-20 14:41:03 +0100

    Welldone

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+/**
+* print_numbers - prints 0 to 9
+* @a: month in number format
+* @b: month in number format
+* Return: void
+*/
+
+
+void print_numbers(void)
+{
+	int t;
+
+	for (t = 0; t < 10; t++)
+	{
+
+		_putchar(t);
+	}
+	_putchar('\n');
+
+}
