0e7f6dc6ad328af3612f112cb008fc9f083fdd6c
HLB <mzeelovegeneral@gmail.com>
2023-07-20 14:32:57 +0100

    Welldone

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+* mul - takes a date and prints how many days are
+* @a: month in number format
+* @b: month in number format
+* Return: void
+*/
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
