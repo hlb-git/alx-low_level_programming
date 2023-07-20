5e2f99f2c3a971c763fcf06ff1ee7143f42fb682
HLB <mzeelovegeneral@gmail.com>
2023-07-20 19:43:05 +0100

    Welldone

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_diagonal - prints 0 to 9
+* @n: the integer argument
+*/
+
+void print_diagonal(int n)
+{
+	int s;
+	int i;
+
+	for (s = 0; s <= n; s++)
+	{
+		i = s;
+
+		for (i = 0; i <= s; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+
+	}
+}
