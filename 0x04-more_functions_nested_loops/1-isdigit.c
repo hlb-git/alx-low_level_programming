fdab47d3f4d28cbc9c22c5998d8eb7d2d1832bcc
HLB <mzeelovegeneral@gmail.com>
2023-07-20 14:16:43 +0100

    Welldone

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+* _isdigit - takes a date and prints how many days are
+* @c: month in number format
+* Return: void
+*/
+
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
