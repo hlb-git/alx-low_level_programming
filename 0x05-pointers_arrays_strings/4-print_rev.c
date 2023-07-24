cd840d86e88b17324a7827548ffb0e39bdbc02ed
HLB <mzeelovegeneral@gmail.com>
2023-07-24 18:39:22 +0100

    prints in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "2-strlen.c"
+
+
+/**
+ * print_rev - converts n to 98
+ * @s: the pointer argument
+ */
+
+void print_rev(char *s)
+{
+	int j = _strlen(s);
+	int n;
+
+	for (n = j - 1; j >= 0; n--)
+	{
+		_putchar(s[n]);
+		if (n < 0)
+		{
+			break;
+		}
+	}
+	_putchar('\n');
+}
