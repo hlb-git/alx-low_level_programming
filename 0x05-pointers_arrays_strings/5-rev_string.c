099c8abcb64a98e3f18c4d430dc742df55759b64
HLB <mzeelovegeneral@gmail.com>
2023-07-24 19:23:55 +0100

    reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "2-strlen.c"
+#include "_putchar.c"
+
+/**
+ * print_rev - converts n to 98
+ * @s: the pointer argument
+ */
+
+void rev_string(char *s)
+{
+	int j = _strlen(s);
+	int n;
+
+	for (n = j - 1; j >= 0; n--)
+	{
+		if (n < 0)
+		{
+			break;
+		}
+		_putchar(s[n]);
+
+	}
+	_putchar('\n');
+}
