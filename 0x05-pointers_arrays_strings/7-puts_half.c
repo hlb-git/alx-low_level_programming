904afd3864c2398c8fa60d6069dbde752858006d
HLB <mzeelovegeneral@gmail.com>
2023-07-24 20:24:21 +0100

    prints half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,7 @@
 
 
 /**
- * puts_half - function name
+ * puts_half- function name
  * @str: the pointer argument
  */
 
@@ -13,6 +13,10 @@ void puts_half(char *str)
 	int j = _strlen(str);
 	int n;
 
+	if (j % 2 == 0)
+	{
+		j = j + 1;
+	}
 	for (n = j / 2; n <= j - 1; n++)
 	{
 		if (n > j - 1)
