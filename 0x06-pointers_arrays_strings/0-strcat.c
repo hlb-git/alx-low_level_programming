777ba1dc24fe454b2d4dddf9fa304bf68aa6973a
HLB <mzeelovegeneral@gmail.com>
2023-07-26 12:40:14 +0100

    concatinates 2 arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strcat - contatinates two strings together
+*
+* Return: this returns the new string
+*/
+
+
+char *_strcat(char *dest, char *src)
+{
+	int p, c;
+
+	for (p = 0; dest[p] != '\0'; p++)
+	{}
+
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[p + 1 + c]	= src[c];
+	}
+	dest[c + p + 1] = '\0';
+	return (dest);
+
+}
