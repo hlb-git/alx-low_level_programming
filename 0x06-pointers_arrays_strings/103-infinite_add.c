ef6c2d21f8b0d1f6d616cc2b0b609cc8f326cbb6
HLB <mzeelovegeneral@gmail.com>
2023-07-27 22:48:13 +0100

    infinite add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* infinite_add - capitalizes srings
+* @n1: first arguement
+* @n2: second argument
+* @r: buffer storage
+* @size_r: buffer size
+* Return: returns capitalized string
+*/
+
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+	return (u);
+}
