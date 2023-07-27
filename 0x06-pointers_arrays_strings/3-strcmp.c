5c9a3d7b57b3ac5c9227706a167ba04b586cc958
HLB <mzeelovegeneral@gmail.com>
2023-07-27 09:26:47 +0100

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-* _strcat - contatinates two strings together
-*
+* _strcmp - contatinates two strings together
+* @s1: first string arguement
+* @s2: second stirng arguement
 * Return: this returns the new string
 */
 
@@ -13,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 
 
 	p = 0;
-	while (s1[p] - s2[p] == 0 && s1[p] != '\0' && s2[p] != '\0' )
+	while (s1[p] - s2[p] == 0 && s1[p] != '\0' && s2[p] != '\0')
 	{
 		p++;
 	}
