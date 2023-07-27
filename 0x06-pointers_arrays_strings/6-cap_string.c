905a4035f91e5a704ffcea7f78eedcf27fbf077a
HLB <mzeelovegeneral@gmail.com>
2023-07-27 13:22:09 +0100

    capitalizes words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* cap_string - contatinates two strings together
+* @u: array to capitalize
+* Return: string to return
+*/
+
+char *cap_string(char *u)
+{
+	int c;
+
+	for (c = 0; u[c] != '\0'; c++)
+	{
+		if (u[c] == ' ' && u[c + 1] >= 'a' && u[c + 1] <= 'z')
+		{
+			u[c + 1] = u[c + 1] - 32;
+		}
+		else if ((u[c] == '\n'|| u[c] == '\t') && u[c + 1] >= 'a' && u[c + 1] <= 'z')
+		{
+			u[c + 1] = u[c + 1] - 32;
+		}
+
+	}
+	return (u);
+}
