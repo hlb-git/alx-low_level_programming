e0ba4fa251598f6240ccae64b31ef2924524fc60
HLB <mzeelovegeneral@gmail.com>
2023-07-27 21:05:52 +0100

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* rot13 - contatinates two strings together
+* @a: array to reverse
+* Return: returns the encoded string.
+*/
+
+char *rot13(char *a)
+{
+	char p[] = "abcdefghijklm";
+	char o[] = "nopqrstuvwxyz";
+	int c, d;
+
+	for (c = 0; a[c] != '\0'; c++)
+	{
+		for (d = 0; p[d] != '\0'; d++)
+		{
+			if (a[c] == p[d])
+			{
+				a[c] = o[d];
+				break;
+			}
+			else if (a[c] == o[d])
+			{
+				a[c] = p[d];
+				break;
+			}
+		}
+	}
+
+	return (a);
+}
