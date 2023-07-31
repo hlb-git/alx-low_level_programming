74d8ce58cb84f4ea31685189b2d336b4ebb1e76a
HLB <mzeelovegeneral@gmail.com>
2023-07-31 22:56:26 +0100

    diagonal addition

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints diagonal summaries
+ * @a: array to use
+ * @size: size of diagonal
+ */
+
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int diagO = 0;
+	int diagT = 0;
+
+	for (i = 0; i < size; i++)
+	{
+	}
+}
