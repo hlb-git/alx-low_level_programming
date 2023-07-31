bd424d037a831106e71fd6fadb05384543c95917
HLB <mzeelovegeneral@gmail.com>
2023-07-31 14:57:51 +0100

    copy to memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - sets memory to array
+ * @dest: array to set
+ * @b: value to set it as
+ * @n: n amount of times
+ * Return: char value
+ */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
