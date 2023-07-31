334f753ed5c5972a1f1453c87e9ec7536f723cbe
HLB <mzeelovegeneral@gmail.com>
2023-07-31 19:28:49 +0100

    update

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - finds string
+ * @s: string to find stuff
+ * @accept: accepting arrays
+ *
+ * Return: returns new value
+ */
+
+
+char *_strpbrk(char *s, char *accept)
+{
+	int t;
+	int j;
+
+	for (t = 0; s[t] != '\0'; t++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[t] == accept[j])
+			{
+				return (s + t);
+			}
+		}
+	}
+	return (s + t);
+}
