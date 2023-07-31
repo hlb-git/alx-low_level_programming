c431fb6397adc27561d8c9b2c868c6cb734b0245
HLB <mzeelovegeneral@gmail.com>
2023-07-31 22:14:08 +0100

    update

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strstr - locates substring
+ * @haystack: string to locte
+ * @needle: substring to locate
+ * Return: returns string at starting sub or null
+ */
+
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (needle);
+}
