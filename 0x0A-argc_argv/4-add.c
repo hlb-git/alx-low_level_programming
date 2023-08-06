fef7ff884ce75f1487afdbb5e6bdd02d9228bcac
HLB <mzeelovegeneral@gmail.com>
2023-08-06 19:25:10 +0100

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define UNUSED(x) (void)(x)
+
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+
+int main(int argc, char  *argv[])
+{
+		UNUSED(argc);
+		UNUSED(argv);
+		return (1);
+}
