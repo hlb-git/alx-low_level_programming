3eb602a1fc32d278cb049a6ec7bac15cd409cf94
HLB <mzeelovegeneral@gmail.com>
2023-08-06 19:54:23 +0100

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+
+int main(int argc, char  *argv[])
+{
+	UNUSED(argc);
+	UNUSED(argv);
+	return (0);
+}
