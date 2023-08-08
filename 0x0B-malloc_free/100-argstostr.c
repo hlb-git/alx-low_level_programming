992431f3f9f89976844c6b27a2e473cfdbd146a6
HLB <mzeelovegeneral@gmail.com>
2023-08-08 23:32:08 +0100

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define UNUSED(x) (void)(x)
+/**
+ * helper - helps function
+ * @word: wordcount
+ * @len: length
+ * @str: string to go through
+ * @s: array you are assigning
+ * Return: char value
+ */
+char **helper(int word, int len, char *str, char **s)
+{
+	UNUSED(word);
+	UNUSED(len);
+	UNUSED(str);
+	UNUSED(s);
+	return (0);
+}
