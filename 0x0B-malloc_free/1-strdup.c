719aa8725acc54bb724f77a915a400742f9d77d4
HLB <mzeelovegeneral@gmail.com>
2023-08-08 12:03:12 +0100

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
 
+/**
  * _strdup - duplicates string
  * @str: string to copy
  * Return: char to newly duplicated string
  */
+
 char *_strdup(char *str)
 {
 	int j = 0;
