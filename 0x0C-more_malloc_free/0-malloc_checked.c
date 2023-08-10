c117f65426b44179ff3a7ef6a1f9407d1cba7df0
HLB <mzeelovegeneral@gmail.com>
2023-08-10 13:52:58 +0100

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#incude "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - checks malloc
+ * @b: integer to check?
+ * Return: a pointer or void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pr;
+
+	pr = malloc(b);
+	if (pr == NULL)
+		exit(98);
+
+	return (t);
+}
