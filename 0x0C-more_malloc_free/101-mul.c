91b25805addfecd4cea224cf51efefdb032dc61d
HLB <mzeelovegeneral@gmail.com>
2023-08-10 18:16:27 +0100

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*main - main function
+*Return: return value for the program
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (num1 == 0 && num2 == 0)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (98);
+	}
+	return (0);
+}
