5448447ce1eca302b8f92ae3813425adc9059c19
Love <mzeelovegeneral@gmail.com>
2023-08-15 19:06:53 +0100

    prints contents of the passed struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* print_dog - the function name
+* @d: the pointer to the struct arguement
+*/
+
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("(nil)\n");
+	if (d->name == NULL)
+		printf("Name: (nil\n)");
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
