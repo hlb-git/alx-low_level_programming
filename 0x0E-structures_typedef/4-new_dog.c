cacb1bd2dec608c5641a650724ead37b48460712
Love <mzeelovegeneral@gmail.com>
2023-08-15 22:20:25 +0100

    creates new dog struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+
+/**
+*new_dog - function name
+*@name: content of the struct
+*@age: another content of the struct
+*@owner: owner parameter in the struct new_dog
+*Return: returns a dog struct type
+*/
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	UNUSED (name);
+	UNUSED (age);
+	UNUSED (owner);
+
+	return (0);
+}
