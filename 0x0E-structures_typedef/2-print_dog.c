b6765f9601601e9acbd0484e4d7770d23611e9e3
Love <mzeelovegeneral@gmail.com>
2023-08-15 19:08:04 +0100

    prints contents of the passed struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		printf("(nil)\n");
 	if (d->name == NULL)
-		printf("Name: (nil\n)");
+		printf("Name: (nil)\n");
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
