cfe2fcbeed9093f7a831fbb2de768d72ddd74344
Love <mzeelovegeneral@gmail.com>
2023-08-16 13:49:48 +0100

    iterates array and executes functions

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+* array_iterator - function name
+* @size: size of the array
+* @action: passed function argurment
+*/
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int count;
+
+	if (array != NULL && size > 0 && action != NULL)
+	{
+		for (count = 0; count < size; count++)
+		{
+			action(array[count]);
+		}
+	}
+}
