5ec766fe44befb92acf502cf75b834faeb60cceb
Love <mzeelovegeneral@gmail.com>
2023-08-16 13:03:59 +0100

    prints name to the standard output

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+* print_name - the function name
+* @name: pointer to the array name
+* @f: function pointer
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
