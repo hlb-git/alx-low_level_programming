18770c023f98108fee5c72e1fd33a43a1e6243f5
Love <mzeelovegeneral@gmail.com>
2023-08-17 16:08:30 +0100

    prints numbers with a specified delimiter

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - function name
+* @seperator: one of the arguements
+* @n: that is the total number of variable arguments
+*/
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+			return;
+		printf("%d", va_arg(args, int));
+		if (i != n - 1)
+			printf("%c ", separator[0]);
+	}
+	va_end(args);
+	printf("\n");
+}
