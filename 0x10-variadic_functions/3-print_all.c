484d746a09c5618c4bba5f83f0cc0f68a7aa9d45
Love <mzeelovegeneral@gmail.com>
2023-08-18 22:09:21 +0100

    prints any variable type

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,39 +16,38 @@ void print_all(const char * const format, ...)
 	va_list args;
 	char *std_format = "cisf";
 
+	if (format == NULL)
+		return;
 	len = strlen(format);
 	va_start(args, format);
-	if (format)
+	j = 0;
+	while (j < len)
 	{
-		j = 0;
-		while (j < len)
+		switch (format[j])
 		{
-			switch (format[j])
-			{
-				case 'c':
-					printf("%c", va_arg(args, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(args, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(args, double));
-					break;
-				case 's':
-					string = va_arg(args, char *);
-					(string == NULL) ? printf("(nil)") : printf("%s", string);
-					break;
-			}
-			k = 0;
-			while (k < 4)
-			{
-				if (format[j] == std_format[k] && j != len - 1)
-					printf(", ");
-				k++;
-			}
-			j++;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				string = va_arg(args, char *);
+				(string == NULL) ? printf("(nil)") : printf("%s", string);
+				break;
 		}
-		printf("\n");
-		va_end(args);
+		k = 0;
+		while (k < 4)
+		{
+			if (format[j] == std_format[k] && j != len - 1)
+				printf(", ");
+			k++;
+		}
+		j++;
 	}
+	printf("\n");
+	va_end(args);
 }
