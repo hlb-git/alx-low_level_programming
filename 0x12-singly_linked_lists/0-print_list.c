5a4cbb8c7c5834dd45553c1f6187c8ae368ac206
Love <mzeelovegeneral@gmail.com>
2023-08-24 15:48:30 +0100

    prints the content of a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - prints the content of a list link
+ * @h: the list_t data type address passed
+ * Return: returns the number of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			count++;
+			h = h->next;
+		}
+	}
+	return (count);
+}
