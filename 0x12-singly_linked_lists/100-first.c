bda6d83bf594e4e1a590927f007a5b8474d7e3e1
Love <mzeelovegeneral@gmail.com>
2023-08-26 04:01:27 +0100

    prints before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * intro - prints the intro message to the screen before the main function
+ */
+
+void intro(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
