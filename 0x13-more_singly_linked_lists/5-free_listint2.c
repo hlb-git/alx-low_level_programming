a6032be29d5e99b2d7bab16eeae722475a8026c4
Love <mzeelovegeneral@gmail.com>
2023-08-29 11:48:09 +0100

    frees the list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the nodes and reset the head to NULL
+ * @head: the list to be freed
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *holder;
+
+	while (head)
+	{
+		holder = head->next;
+		free(head);
+		*head = holder;
+	}
+	head = NULL;
+}
