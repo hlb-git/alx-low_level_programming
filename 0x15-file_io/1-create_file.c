9ac4cb5216930a2e9cdcba5d4013cc604e2db5f5
Love <mzeelovegeneral@gmail.com>
2023-09-04 19:00:44 +0100

    creates a file and writes in it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fil_des = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	fil_des = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fil_des == -1)
 		return (-1);
 	if (text_content)
