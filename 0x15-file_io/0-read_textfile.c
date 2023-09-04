c491225cfeb8c01e0a6590038e708c2c6377f6b4
Love <mzeelovegeneral@gmail.com>
2023-09-04 15:49:51 +0100

    reads a file then writes the content to standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads the content of a file
+ * @filename: the file name that's gonna be used
+ * @letters: the total number of letters in the file
+ * Return: the number of leters it was able to print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t tot_read, fildes, tot_wrote;
+	void *buffer;
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (-1);
+
+	if (filename == NULL)
+		return (0);
+	fildes = open(filename, O_RDWR);
+	if (fildes == -1)
+		return (0);
+
+	tot_read = read(fildes, buffer, letters);
+
+	tot_wrote = write(STDOUT_FILENO, buffer, tot_read);
+	if (tot_wrote == -1)
+		return (0);
+
+	return (tot_wrote);
+}
