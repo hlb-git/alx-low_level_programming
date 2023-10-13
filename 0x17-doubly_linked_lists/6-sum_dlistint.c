c8dda8de20be09abf58b1be25f561954db25bd43
Love <mzeelovegeneral@gmail.com>
2023-10-13 13:39:11 +0100

    sum up values in doubly linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums up values in doubly linked list
+ * @head: head of address to get sum
+ * Return: sum value
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	const dlistint_t *temp;
+	int sum = 0;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
