62ce39e99095a5f24d63386eec56e0af7dd1e68d
Love <mzeelovegeneral@gmail.com>
2023-11-03 10:47:44 +0100

    add element to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	new->next = NULL;
+	new->next = ht->array[index];
 	ht->array[index] = new;
 
 	return (1);
