381243db0181f6ad7d4926e42f0fe5c6bc16b8c1
Love <mzeelovegeneral@gmail.com>
2023-11-03 17:24:14 +0100

    get hash value from hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves the value stored in a hash table
+ * @ht: the hash table to use
+ * @key: the key to each value
+ * Return: returns the value of the key
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (!ht || !key || !ht->size)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index])
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+			return (ht->array[index]->value);
+	}
+	return (NULL);
+}
